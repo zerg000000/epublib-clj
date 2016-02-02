ccd32d554af77e350a5d0e0590f1938c5c0cfca2
Albert Lai <kayulai2@gmail.com>
2016-02-02 22:55:40 +0800

    upgrade deps

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/zerg000000/epublib-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.2.374"]
                  [prismatic/schema "1.0.4"]
                  [nl.siegmann.epublib/epublib-core "3.1"]]
